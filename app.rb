8afc2e1bc214e93a19a521a455529934f3460844
sovetbek <akmatkulovs991@gmail.com>
2021-12-12 14:37:35 +0300

    Add file index.erb and welcome.erb

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
 get '/' do
-  "El Camino" 
+  erb :index
+end
+
+post '/' do
+  @login = params[:aaa]
+  @password = params[:bbb]
+
+  if @login == 'admin' && @password == 'secret'
+    erb :welcome
+  else
+    @acces_denied = "Access Denied!"
+    erb :index
+  end
 end
